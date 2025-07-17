cb7cd4a86f62dc8c8d2d1e0c44e09b8291383b2c
Aqori <117046442+aqori@users.noreply.github.com>
2025-07-17 13:23:54 +0700

    Add dynamicexchange.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ internal/dynamicexchange/dynamicexchange.go
@@ -0,0 +1,36 @@
+// internal/dynamicexchange/dynamicexchange.go
+package dynamicexchange
+
+import (
+"log"
+"os"
+)
+
+type App struct {
+verbose bool
+logger *log.Logger
+}
+
+func NewApp(verbose bool) *App {
+app := &App{
+verbose: verbose,
+logger: log.New(os.Stdout, "", log.LstdFlags),
+}
+
+if verbose {
+app.logger.SetPrefix("[DEBUG] ")
+} else {
+app.logger.SetPrefix("[INFO] ")
+}
+
+return app
+}
+
+func (a *App) Run() error {
+a.logger.Printf("Starting %s processing", "DynamicExchange")
+
+// Add your implementation here
+
+a.logger.Println("Processing completed successfully")
+return nil
+}
